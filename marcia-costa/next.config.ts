77eb670053c34f3eb3e17322099d0c90ab0c7a06
Alexandre802 <alexandrepfernandes46@gmail.com>
2026-09-02 00:16:17 -0300

    Libera somente as fontes oficiais das fotos do cardápio

--- marcia-costa/next.config.ts
+++ marcia-costa/next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static-images.ifood.com.br",
+      },
+      {
+        protocol: "https",
+        hostname: "instadelivery-public.nyc3.cdn.digitaloceanspaces.com",
+      },
+    ],
   },
 };
 
